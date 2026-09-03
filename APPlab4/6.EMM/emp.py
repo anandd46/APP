23ce9f6faf16f4c4afd68ec14282603390cd7895
Anand_d <anandrl120@gmail.com>
2026-09-03 12:42:56 +0530

    Update emp.py

--- APPlab4/6.EMM/emp.py
+++ APPlab4/6.EMM/emp.py
@@ -1,1 +1,18 @@
 
+def calculate_salary(basic_salary, allowance):
+    return basic_salary + allowance
+
+
+def calculate_bonus(salary, experience):
+    if experience >= 5:
+        return salary * 0.10
+    else:
+        return salary * 0.05
+
+
+def employee_details(name, department, salary, experience):
+    print("Employee Details")
+    print("Name:", name)
+    print("Department:", department)
+    print("Salary:", salary)
+    print("Experience:", experience)
