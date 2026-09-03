3f363efd69c89c1f776e10afd068d454d50ca154
Anand_d <anandrl120@gmail.com>
2026-09-03 12:43:21 +0530

    Implement employee salary and bonus calculations

---
+++ APPlab4/6.EMM/main.py
@@ -0,0 +1,10 @@
+from employee import calculate_salary
+from employee import calculate_bonus
+from employee import employee_details
+
+salary = calculate_salary(50000, 10000)
+bonus = calculate_bonus(salary, 6)
+
+employee_details("Likhitha", "IT", salary, 6)
+
+print("Bonus:", bonus)
